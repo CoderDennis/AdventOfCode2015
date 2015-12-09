210973af8bdff457d06540f8ad851c424b72a2f3
CoderDennis <dodava@gmail.com>
2015-12-08 23:21:48 -0600

    day2 complete

--- day2/day2.exs
+++ day2/day2.exs
@@ -1,10 +1,16 @@
 defmodule Day2 do
-  def area_of_sides([l, w, h]), do: [l*w, w*h, h*l]
-  def area_of_sides(string) do
+
+  def sides(string) do
     string
     |> String.strip
     |> String.split("x")
     |> Enum.map(&String.to_integer/1)
+  end
+
+  def area_of_sides([l, w, h]), do: [l*w, w*h, h*l]
+  def area_of_sides(string) do
+    string
+    |> sides
     |> area_of_sides
   end
 
@@ -22,6 +28,19 @@ defmodule Day2 do
      |> Enum.map(&(&1*2))
      |> Enum.sum) + smallest
   end
+
+  def ribbon(string) do
+    sides =
+    (string
+     |> sides
+     |> Enum.sort)
+    ribbon_to_wrap(sides) + ribbon_for_bow(sides)
+  end
+
+  defp ribbon_to_wrap([a, b, _]), do: a+a+b+b
+
+  defp ribbon_for_bow([a, b, c]), do: a*b*c
+
 end
 
 # File.stream!("input.txt", [:read, :utf8]) |> Stream.map(&Day2.paper_required/1) |> Enum.sum
