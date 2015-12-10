b38be4fe43c346f05830613cbd264e7ae2192bcb
CoderDennis <dodava@gmail.com>
2015-12-09 20:01:47 -0600

    day 3 complete

--- day3/day3.exs
+++ day3/day3.exs
@@ -12,6 +12,23 @@ defmodule Day3 do
   def houses([?<|t], [{x,y}|_] = visited), do: houses(t, [{x-1,y}|visited])
   def houses([?^|t], [{x,y}|_] = visited), do: houses(t, [{x,y+1}|visited])
   def houses([?v|t], [{x,y}|_] = visited), do: houses(t, [{x,y-1}|visited])
+
+  def houses2(input) do
+    {a, b} = input
+    |> String.to_char_list
+    |> Enum.with_index
+    |> Enum.partition(fn {_e, i} -> rem(i,2) == 0 end)
+    santa = a
+    |> Enum.map(fn {e, _i} -> e end)
+    |> houses([{0,0}])
+    robo_santa = b
+    |> Enum.map(fn {e, _i} -> e end)
+    |> houses([{0,0}])
+    Enum.concat(santa, robo_santa)
+    |> Enum.uniq
+    |> Enum.count
+  end
+
 end
 
 # File.read!("input.txt") |> Day3.houses
